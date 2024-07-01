cdad81b6c3e74d578310e3f1fc97cee9f017a8df
Welberth <welbertholiveira250@gmail.com>
2024-07-01 15:32:07 -0300

    Mostrando dano nos inimigos
    Adicionado um damage
    Adicionado um animationPlayer
    Animação do dano feita
    Posicionou o dano nos inimigos e mostra ao tomar dano

--- Tiny Swords/enemies/enemy.gd
+++ Tiny Swords/enemies/enemy.gd
@@ -4,6 +4,12 @@ extends Node2D
 
 @export var health: int = 10
 @export var death_prefab: PackedScene
+var damage_digit_prefab: PackedScene
+
+@onready var damage_digit_marker = $DamageDigitMarker
+
+func _ready():
+	damage_digit_prefab = preload("res://misc/damage_digit.tscn")
 
 
 func damage(amount: int):
@@ -17,6 +23,14 @@ func damage(amount: int):
 	tween.set_trans(tween.TRANS_QUINT)
 	tween.tween_property(self, "modulate", Color.WHITE, 0.3)
 	
+	# Criar um Damage Digit
+	var damage_digit = damage_digit_prefab.instantiate()
+	damage_digit.value = amount
+	if damage_digit_marker:
+		damage_digit.global_position = damage_digit_marker.global_position
+	else:
+		damage_digit.global_position = global_position
+	get_parent().add_child(damage_digit)
 	
 	# Processar morte
 	if health <= 0:
