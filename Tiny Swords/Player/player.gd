bfe22e86801e86e5844ccc9bf68bf97854926be9
Welberth <welbertholiveira250@gmail.com>
2024-07-02 19:20:50 -0300

    Adicionando carne na interface
    Carne adicionada na interface do jogo

--- Tiny Swords/Player/player.gd
+++ Tiny Swords/Player/player.gd
@@ -29,6 +29,13 @@ var attack_cooldown: float = 0.0
 var hitbox_cooldown: float = 0.0
 var ritual_cooldown: float = 0.0
 
+signal meat_collected(value: int)
+
+
+func _ready():
+	GameManager.player = self 
+
+
 func _process(delta: float):
 	GameManager.player_position = position
 	
--- Tiny Swords/UI/game_ui.gd
+++ Tiny Swords/UI/game_ui.gd
@@ -1,10 +1,16 @@
 extends CanvasLayer
 
 @onready var timer_label: Label = %TimerLabel
-@onready var gold_label: Label = %GoldLabel
 @onready var meat_label: Label = %MeatLabel
 
+
 var time_elapsed: float = 0.0
+var meat_counter: int = 0
+
+
+func _ready():
+	GameManager.player.meat_collected.connect(on_meat_collected)
+	meat_label.text = str(meat_counter)
 
 
 func _process(delta: float):
@@ -15,3 +21,7 @@ func _process(delta: float):
 	var minutes: int = time_elapsed_in_seconds / 60 
 	timer_label.text = "%02d:%02d" % [minutes, seconds]
  
+
+func on_meat_collected(value: int):
+	meat_counter += 1
+	meat_label.text = str(meat_counter)
